1ad5d2d31271f03ceeeb8197ec29436731d2c581
Chris Busillo <info@shinycomputers.com>
2025-04-27 11:12:24 -0400

    Add new submodule for disabling Odoo Online module
    Introduced a new git submodule "disable_odoo_online" to manage disabling the Odoo Online functionality. This addition aims to enhance customization and streamline system behavior for specific use cases.

--- models/publisher_warranty_contract.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-class PublisherWarrantyContract(models.AbstractModel):
-    _name = "publisher_warranty.contract"
-    _description = "Publisher Warranty Contract"
-
-    def update_notification(self, cron_mode: bool = True) -> bool:
-        return True
