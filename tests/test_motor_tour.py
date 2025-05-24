74f5bb222df476295214fd90feb421ab1ecd3ecc
Chris Busillo <info@shinycomputers.com>
2025-05-24 13:16:09 -0400

    Add new tour user for motor frontend tests and expand test imports
    Introduce a dedicated user with specific credentials for motor frontend tours to improve test isolation. Also, update test module imports to include additional service-related test files for broader test coverage.

--- tests/test_motor_tour.py
+++ tests/test_motor_tour.py
@@ -1,7 +1,34 @@
+from odoo import SUPERUSER_ID
 from odoo.tests import HttpCase, tagged
 
 
 @tagged("post_install", "-at_install")
 class TestMotorFrontendTour(HttpCase):
+    _password = "tour_password8glue34ji"
+    _login = "tour_admin"
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        test_admin = self.env.ref("base.group_system")
+        self.tour_user = (
+            self.env["res.users"]
+            .with_user(SUPERUSER_ID)
+            .create(
+                {
+                    "name": "Tour Administrator",
+                    "login": self._login,
+                    "password": self._password,
+                    "groups_id": [(6, 0, [test_admin.id])],
+                }
+            )
+        )
+
     def test_motor_frontend_tour(self) -> None:
-        self.start_tour("/web", "motor_frontend_tour", login="admin")
+        self.start_tour(
+            "/web",
+            "motor_frontend_tour",
+            login=self._login,
+            password=self._password,
+            modules="product_connect",
+        )
